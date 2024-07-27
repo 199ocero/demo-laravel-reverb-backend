1e592a3a2a9ef633e785e0c8234d6a981c51e8ee
199ocero <199ocero@gmail.com>
2024-07-27 18:16:30 +0800

    added throttle middleware

--- routes/api.php
+++ routes/api.php
@@ -4,10 +4,10 @@ use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'register'])->name('register')->middleware('throttle:3,1');
 
-Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/login', [AuthController::class, 'login'])->name('login')->middleware('throttle:3,1');
 
-Route::group(['middleware' => 'auth:sanctum'], function () {
+Route::group(['middleware' => ['auth:sanctum', 'throttle:3,1']], function () {
     Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 });
