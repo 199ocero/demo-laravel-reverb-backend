9eb328f128df7a81793e53a6813652bf5fbde1fd
199ocero <199ocero@gmail.com>
2024-07-28 14:21:08 +0800

    added user api and fix throttle

--- routes/api.php
+++ routes/api.php
@@ -2,13 +2,14 @@
 
 use App\Enums\TokenAbility;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [AuthController::class, 'register'])->name('register')->middleware('throttle:3,1');
 
 Route::post('/login', [AuthController::class, 'login'])->name('login')->middleware('throttle:3,1');
 
-Route::group(['middleware' => ['auth:sanctum', 'throttle:3,1']], function () {
+Route::group(['middleware' => ['auth:sanctum', 'throttle:60,1']], function () {
 
     // Route for issue access token ability only
     Route::group(['middleware' => 'ability:'.TokenAbility::ISSUE_ACCESS_TOKEN->value], function () {
@@ -18,5 +19,7 @@ Route::group(['middleware' => ['auth:sanctum', 'throttle:3,1']], function () {
     // Route for access api ability only
     Route::group(['middleware' => 'ability:'.TokenAbility::ACCESS_API->value], function () {
         Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+
+        Route::get('/user/me', [UserController::class, 'me'])->name('user.me');
     });
 });
